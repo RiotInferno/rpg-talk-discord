8de17627b61c64b16f09e62191abecae57ca8405
Karl Scheer <waffles279@gmail.com>
2019-10-23 22:13:03 -0600

    Update index.ts
    Remove pointer to rules PDF and instead go to rules channel

--- index.ts
+++ index.ts
@@ -460,7 +460,7 @@ bot.on('guildMemberAdd', async (member) => {
             `There are many more channels beyond the ${defaultRoleNames.length + 1} default ones. ` +
             `There are **${allChannels(member.guild).length}** in total!\n\n` +
             `Discover them all by entering the **/channels** command here.\n\n` +
-            `Be sure to review the Code of Conduct at https://rpg-talk.com/Code%20of%20Conduct.pdf.`)
+            `Be sure to review the Code of Conduct in our #rules channels`)
             .catch(err => console.log(err))
     } catch (error) { }
 });
