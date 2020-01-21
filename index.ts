26c7427827c0d4b7a5e4dcded46c5ab6854b4fbd
Karl Scheer <waffles279@gmail.com>
2020-01-21 07:57:34 -0700

    Error-handling for channel topic

--- index.ts
+++ index.ts
@@ -253,7 +253,11 @@ channelsCommand.run = async (message: CommandMessage, args: string): Promise<any
 
             if (typeof channelForRole !== 'undefined' && channelForRole.type == 'text')
             {
-                channelTopic = " - " + (channelForRole.topic || '(no topic)')
+              try {
+                  channelTopic = " - " + (channelForRole.topic || '(no topic)')
+              } catch (error) {
+                  console.log(error);
+              }
             }
 
             line += allRoles[i] + channelTopic
