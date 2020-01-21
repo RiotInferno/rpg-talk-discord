5dacb9ae26bc92167576b80baf5aea9613337c76
Karl Scheer <waffles279@gmail.com>
2020-01-20 21:39:52 -0700

    Show topic

--- index.ts
+++ index.ts
@@ -248,7 +248,7 @@ channelsCommand.run = async (message: CommandMessage, args: string): Promise<any
         _.range(allRoles.length).forEach(i => {
             var line = '';
 
-            var channelForRole = detectGuild(bot, message).channels.find('name', allRoles[i]).name;
+            var channelForRole = detectGuild(bot, message).channels.find('name', allRoles[i]).topic;
 
             line += allRoles[i] + "\t-\t" + channelForRole
             //line += detectGuild(bot, message).channels.find('name', allRoles[i]).topic
