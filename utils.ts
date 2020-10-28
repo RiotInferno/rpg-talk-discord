5a7b2c6788d08a2be2d22c01187a1a64f4d53b89
riotinferno <RiotInferno@users.noreply.github.com>
2020-10-28 14:57:52 -0500

    Revert "Merge remote-tracking branch 'upstream/master' into testing"
    This reverts commit 728f19829f2ed080832c2e1f3171be0cca65dc31, reversing
    changes made to accbd16745e43a540c135b8459fab08eae5b42a3.

--- utils.ts
+++ utils.ts
@@ -1,4 +1,4 @@
-import { CommandoClient, CommandMessage } from 'discord.js-commando'
+import { CommandoClient, CommandoMessage } from 'discord.js-commando'
 import { TextChannel, Guild, Role } from 'discord.js';
 import * as _ from 'lodash'
 
@@ -8,23 +8,23 @@ export const blacklisted = (process.env.BLACKLIST || '')
     .map(channel => channel.toLowerCase())
     .filter(channel => channel.length > 0);
 
-export function detectGuild(bot: CommandoClient, message: CommandMessage): Guild {
+export function detectGuild(bot: CommandoClient, message: CommandoMessage): Guild {
   if (message.guild) {
     return message.guild;
   } else {
-    return bot.guilds.first();
+    return bot.guilds.cache.first();
   }
 }
 
 export function mapToRoles(channelNames: string[], guild: Guild): Role[] {
   return channelNames
-    .map(name => guild.roles.find('name', name))
+    .map(name => guild.roles.cache.find(role => role.name === name))
     .filter(role => role)
     .filter(role => !_.includes(blacklisted, role.name.toLowerCase()));
 }
 
 export function channelHasRole(channelName: string, guild: Guild): boolean {
-  let role = guild.roles.find('name', channelName);
+  let role = guild.roles.cache.find(role => role.name === channelName);
   if (role != null && !blacklisted.includes(role.name.toLowerCase()))
   {
     return true;
@@ -33,12 +33,12 @@ export function channelHasRole(channelName: string, guild: Guild): boolean {
 }
 
 export function allChannels(guild: Guild): string[] {
-  const channels = guild.channels
+  const channels = guild.channels.cache
     .filter(channel => channel.type == 'text')
     .filter(channel => !_.includes(blacklisted, channel.name.toLowerCase()))
     .map(channel => channel.name);
 
-  return guild.roles
+  return guild.roles.cache
     .filter(role => !_.includes(blacklisted, role.name.toLowerCase()))
     .map(role => role.name)
     .filter(role => _.includes(channels, role))
@@ -49,7 +49,7 @@ export function cleanupChannelName(channelName: string, guild: Guild): string {
   if (channelName.includes('<#')) {
     channelName = channelName.substr(2,)
     channelName = channelName.substr(0, channelName.length - 1)
-    return guild.channels.find('id', channelName).name;
+    return guild.channels.cache.find(channel => channel.id === channelName).name;
   } else if (channelName.startsWith('#')) {
     return channelName.substr(1).toLowerCase();
   } else {
@@ -59,7 +59,7 @@ export function cleanupChannelName(channelName: string, guild: Guild): string {
 
 export function mapToChannels(channelNames: string[], guild: Guild): TextChannel[] {
   return channelNames
-    .map(name => guild.channels.find('name', name))
+    .map(name => guild.channels.cache.find(channel => channel.name === name))
     .filter(channel => channel)
     .filter(channel => !_.includes(blacklisted, channel.name.toLowerCase())) as TextChannel[];
 }
